55575116b9e3e67ef3f16ff50cec2d3d0664a1a9
Murilo Rodrigues <murilosisnando2003@hotmail.com>
2018-07-28 16:59:06 -0300

    vai

--- sirh/wsgi.py
+++ sirh/wsgi.py
@@ -11,7 +11,9 @@ import os
 from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sirh.settings")
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
